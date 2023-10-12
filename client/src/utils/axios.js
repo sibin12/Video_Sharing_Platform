24b7d691379df1189755ba1672596b9da2efe7cd
Sibin Silu <sibinsilu001@gmail.com>
2023-10-12 09:39:37 +0530

    renamed api and builded

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 const token = localStorage.getItem('token');
 const adminToken = localStorage.getItem('admintoken')
-const URL='https://loopnet.gadgetgalaxy.live/api'
+const URL='https://loopnet.gadgetgalaxy.live/'
 
 const userInstance = axios.create({
     baseURL:`${URL}users`,
